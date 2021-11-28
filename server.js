d8c2e25690d55cfa24546152e1b4b3975e4e772f
JavierVSM <javier.vsm@gmail.com>
2021-11-28 11:56:48 -0600

    First commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require( 'express' );
+const app = express();
+
+app.use(express.static(__dirname + "/static"));
+
+app.set('views', __dirname + '/views'); 
+app.set('view engine', 'ejs');
+
+app.get("/cars", function (request, response){
+  response.render('cars');
+})
+
+app.get("/cats", function (request, response){
+  response.render('cats');
+})
+
+app.get("/form", function (request, response){
+  response.render('form');
+})
+
+app.listen(8000, function() {
+    console.log("listening on port 8000");
+  })
